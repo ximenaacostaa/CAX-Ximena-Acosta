c89a728289fee2154f1f5ba9e94d7fa347fe38a9
ximenaacostaa <104370560+ximenaacostaa@users.noreply.github.com>
2022-06-01 17:02:58 -0500

    Add files via upload

---
+++ Python/1er Trimeste/Constants.py
@@ -0,0 +1,17 @@
+x=10
+
+jamon=10
+
+justo=2345
+
+excusa= 'puedo ir al baño?'
+
+print(x)
+print(jamon)
+print(excusa)
+
+cØmØ3st4n=20
+
+print(cØmØ3st4n)
+
+print('el precio del jamon es:', jamon )
---
+++ Python/1er Trimeste/Math operators .py
@@ -0,0 +1,35 @@
+
+math1=100
+math2= 90
+math3= 96
+total= math1+math2+math3 
+math=total/3
+print(math)
+
+science1=98
+science2=89
+science3=92
+total=science1+science2+science3
+science= total/3
+print(science)
+
+fisica1=90
+fisica2=93
+fisica3=94
+total=fisica1+fisica2+fisica3
+fisica=total/3
+print(fisica)
+
+pe1=100
+pe2=98
+pe3=99
+total=pe1+pe2+pe3
+pe=total/3
+print(pe)
+
+mct1=90
+mct2=86
+mct3=90
+total=mct1+mct2+mct3
+mct=total/3
+print(mct)
