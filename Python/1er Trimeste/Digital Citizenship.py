cd9fc3db1341938949ce6c7bafd91a962ed9d290
ximenaacostaa <104370560+ximenaacostaa@users.noreply.github.com>
2022-06-03 13:54:12 -0500

    Add files via upload

---
+++ Python/1er Trimeste/Digital Citizenship.py
@@ -0,0 +1,20 @@
+print("how can we act with empathy and positivity when we're onlyne?")
+print("thinking about what we are going to do")
+print("digital_dilema")
+
+digital_dilema:"a situation in digitak life where it's not always clear what's best to do"
+
+print("the actions of the girl in my opinion where bad, i think she would have to just told him not posting it in internet")
+
+
+identif="a girl and a boy they ate facing syber bulling"
+print(identif)
+
+feel="the boy is feelig embarassed and bed and the girl first funny and then bad"
+print(feel)
+
+imagine="she go to his house and say sorry, he not sending the letter, he talking with her in person"
+print(imagine)
+
+say="if he didn't say nothing no body would make fun of him buthe will be hidding his fellings and it will make him fell bad" 
+print(say)
---
+++ Python/1er Trimeste/Input .py
@@ -0,0 +1,50 @@
+print('write your name ')
+name=input()
+print('hi :)',name) 
+
+
+print('write your age')
+age =input()
+print('you are',age)
+
+
+print('where are you from? ')
+fromm =input()
+print('you live in ',fromm)
+
+
+print('what is your favorite flavor of ice cream ')
+flavor =input()
+print( 'your favorite ice cream flavor iss' ,flavor)
+
+
+print('what is your favorite movie?')
+movie =input()
+print('you like watching ',movie )
+
+
+print('what do you like doing? ')
+doing =input()
+print('you like',doing)
+
+
+print('do you prefer cats or dogs?')
+orr=input()
+print('you prefer',orr)
+
+print('what is your favorit food?')
+food=input()
+print('you like eating',food)
+
+
+print('what is your favorite color?')
+color=input()
+print('your favorite color is',color)
+
+print('what do you prefer day or night?')
+day=input()
+print('you prefer',day)
+
+
+
+
