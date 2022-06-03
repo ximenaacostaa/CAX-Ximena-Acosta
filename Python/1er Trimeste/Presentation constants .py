d67ba7ee189ab0c7df01eb6687e091710bea10b7
ximenaacostaa <104370560+ximenaacostaa@users.noreply.github.com>
2022-06-03 13:44:50 -0500

    Add files via upload

---
+++ Python/1er Trimeste/Presentation constants .py
@@ -0,0 +1,11 @@
+name='ximenita'
+grade='in 9 grade'
+food='spaguetti'
+dessert='ice cream'
+color='white'
+
+print('my name is:',name)
+print('i am:',grade )
+print('my favorite food is:',food)
+print('my favorite dessert is:',dessert)
+print('my favorite color is:',color)
