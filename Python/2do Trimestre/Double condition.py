91e2a68f20fda680e9abd7510148190ea1ad41ef
ximenaacostaa <104370560+ximenaacostaa@users.noreply.github.com>
2022-06-28 10:21:02 -0500

    Add files via upload

---
+++ Python/2do Trimestre/Double condition.py
@@ -0,0 +1,14 @@
+#####volver el codigo interactivo pues que se ingrese el numero a mano
+age=int(input('test:'))
+
+if(age<10):
+  print('smaller')
+
+if((age>=10) and (age<=20)):
+  print("yes")
+
+
+if(age>20):
+  print('bigger')
+
+print("finis")
---
+++ Python/2do Trimestre/if condition.py
@@ -0,0 +1,9 @@
+num= input("ingrese un numero:")
+
+if num < 10:
+  print('Smaller')
+
+if num > 20:
+  print('Bigger')
+
+print('Finis')
